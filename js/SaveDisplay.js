70d9dc0a256a50a53aabc109efb602ce7a1c7f8e
YungLawn <cteelon@usca.edu>
2020-11-16 13:22:36 -0500

    2 files modified

--- js/SaveDisplay.js
+++ js/SaveDisplay.js
@@ -1,9 +1,45 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {};
+var firebaseConfig = {
+  apiKey: "AIzaSyA6BlQyU38IZj3MNizq4KeKttMvqnNuftU",
+  authDomain: "grocerylist-41c62.firebaseapp.com",
+  databaseURL: "https://grocerylist-41c62.firebaseio.com",
+  projectId: "grocerylist-41c62",
+  storageBucket: "grocerylist-41c62.appspot.com",
+  messagingSenderId: "899318465752",
+  appId: "1:899318465752:web:bd2ef218b10ac6c33b03ad",
+  measurementId: "G-52120D3SXT"
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // save the data
+$(".sampleSurvey input[type='submit']").click(function(e){
+  e.preventDefault();
+
+  //get value of the form using serializeArray method
+  var data = $("form").serializeArray();
+  console.log(data);
+
+  var inputJson = {};
+  for(var i = 0; i < data.length;i++){
+
+    var name = data[i]["name"];
+    var value = data[i]["value"];
+    // console.log(name + " " + value);
+    inputJson[name] = value;
+  }
+  
+  console.log(inputJson);
+
+  firebase
+    .firestore()
+    .collection("survey")
+    .add(inputJson);
+
+  /* clear the entry */
+  $("form")[0].reset();
+
+});
 
 // update the result in table
--- js/hotel-json.js
+++ js/hotel-json.js
@@ -10,30 +10,33 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+// var testJson = {};
+// testJson['lastname'] = "Teelon";
+// console.log(testJson);
 
 // enter data in
 $("input[type='button']").click(function(e) {
   //get the value of form
   var inputdata = $("form").serializeArray();
+  console.log(inputdata);
   var inputJson = {};
   for (var i = 0; i < inputdata.length; i++) {
     var name = inputdata[i]["name"];
     var value = inputdata[i]["value"];
-    console.log(name + " " + value);
+    // console.log(name + " " + value);
     inputJson[name] = value;
   }
 
   console.log(inputJson);
-  //var night = parseInt(inputJson["num"]);
-  // var cost = parseInt(inputJson["room"].slice(-3));
-  // console.log("The cost for this reservation is " + night * cost);
+  var night = parseInt(inputJson["num"]);
+  var cost = parseInt(inputJson["room"].slice(-3));
+  console.log("The cost for this reservation is " + night * cost);
 
-  /* save the data to database 
+  /* save the data to database */
   firebase
     .firestore()
     .collection("hotelreservation")
     .add(inputJson);
-*/
 
   /* clear the entry */
   $("form")[0].reset();
